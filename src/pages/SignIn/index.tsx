b7be93f1e64b4815f59edf5bde5b4e88f655bf4e
Guilherme Ferreira <guilhermefos.developer@gmail.com>
2020-07-22 15:41:06 -0300

    First Commit

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Image} from 'react-native';
+
+import {Container} from './styles';
+
+import logo from '../../assets/logo.png';
+
+const SingIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logo} />
+    </Container>
+  );
+};
+
+export default SingIn;
